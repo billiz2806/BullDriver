71d3519bb174f32d7244be930c4fc480488d85bb
Billy Salazar <79593247+billiz2806@users.noreply.github.com>
2022-11-03 09:42:04 -0500

    Agregar archivos de proyecto.

---
+++ BullDriver/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BullDriver.Services;
+using BullDriver.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BullDriver
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BullDriver/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BullDriver.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BullDriver.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
